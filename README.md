6120a99b8dc4148fdf15b9fd8a846fd6ff5a9542
Oda <oda@member.fsf.org>
2016-11-07 00:09:38 -0200

    trololololo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ppd42
+# PPD42
+
+Yet another example of PPD42 with arduino, I think it is less crap then the others I've found. The mathematical formulae were copied from the spec, I have no idea if they are right.
+
+I've tested with esp8266 (wemos), using ports D6 and D7 for P10 and P25, respectively, with a simple tension divider:
+
+                   R                         R      R
+PPD42 Pin i -----vvvvv----- Wemos Dj  -----vvvvv--vvvvv----- GND
+
+where R = 4k7 and (i,j) \in {(2,6),(4,7)}
+
+
---
+++ ppd42.ino
@@ -0,0 +1,64 @@
+#define P25 D7
+#define P10 D6
+
+#define SAMPLELENGTH 30000 // 30seg
+
+unsigned long sampleTime = 0;
+
+volatile unsigned long p25Time = 0;
+volatile unsigned long p25Duration = 0;
+volatile unsigned long p25LowLength = 0;
+
+volatile unsigned long p10Time = 0;
+volatile unsigned long p10Duration = 0;
+volatile unsigned long p10LowLength = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(P25,INPUT);
+  pinMode(P10,INPUT);
+  attachInterrupt(digitalPinToInterrupt(P25), P25Change, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(P10), P10Change, CHANGE);
+  sampleTime = millis();
+}
+
+void P25Change(){
+  unsigned long ms = micros();
+  if(digitalRead(P25)==HIGH){
+    p25Duration = ms - p25Time;
+    p25LowLength += p25Duration;
+  }else{ // falling
+    p25Time = ms;
+  }
+}
+void P10Change(){
+  unsigned long ms = micros();
+  if(digitalRead(P10)==HIGH){
+    p10Duration = ms - p10Time;
+    p10LowLength += p10Duration;
+  }else{ // falling
+    p10Time = ms;
+  }
+}
+
+void loop() {
+  if ((millis()-sampleTime) > SAMPLELENGTH){    
+    Serial.print("P25 concentration: ");
+    Serial.print(calcConcentration(p25LowLength));
+    Serial.println(" pcs/0.01cf");
+    p25LowLength = 0;
+
+    Serial.print("P10 concentration: ");
+    Serial.print(calcConcentration(p10LowLength));
+    Serial.println(" pcs/0.01cf");
+    p10LowLength = 0;
+
+    sampleTime = millis();
+  }
+}
+
+// untested blackmagic from spec
+float calcConcentration(unsigned long lowLength){
+  float ratio = lowLength/(SAMPLELENGTH*10.0);
+  return 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
+}
